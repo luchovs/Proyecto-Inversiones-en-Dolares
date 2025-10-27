9c8726116718e12b46abf56859bcc7bdf08a9864
luchovs <lucianosavia15@gmail.com>
2025-10-27 11:26:46 -0300

    Sacamos archivos innecesarios

--- Backend/Conexión/tests/test_backend.py
+++ Backend/Conexión/tests/test_backend.py
@@ -7,7 +7,6 @@ def test_home(client):
 
 
 def test_registro_missing_data(client):
-    # Falta información => debería fallar
     response = client.post("/registro", json={})
     assert response.status_code in (400, 500)
     data = response.get_json()
@@ -36,24 +35,3 @@ def test_get_usuarios(client):
 def test_editar_usuario_missing_data(client):
     response = client.put("/editar_usuario", json={})
     assert response.status_code == 200 or response.status_code == 500
-
-def test_eliminar_usuario_missing_data(client):
-    response = client.delete("/eliminar_usuario", json={})
-    # Si no se envía usuario debería fallar
-    assert response.status_code in (400, 404, 500)
-
-
-def test_agregar_inversion_missing_data(client):
-    response = client.post("/agregar_inversion", json={})
-    assert response.status_code in (400, 404, 500)
-
-
-def test_obtener_inversiones(client):
-    response = client.get("/inversiones")
-    # Si la ruta no existe => 404 también está bien
-    assert response.status_code in (200, 404, 500)
-
-
-def test_update_inversion_missing_data(client):
-    response = client.put("/editar_inversion", json={})
-    assert response.status_code in (400, 404, 500)
